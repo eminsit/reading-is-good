d547195e3d6bf542d7873c2f134249cd72115fb8
Emin Sit <eminsit@gmail.com>
2020-11-07 18:21:19 +0300

    Add h2 configuretions and startup db scripts.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO BOOKS (title, author_name, count, price) VALUES
+  ('Sefiller', 'Victor Hugop', 21, 10),
+  ('Vadideki Zambak', 'Maksim Gorki ', 32, 20),
+  ('Beyaz Diş', 'Jack London ', 4, 30),
+  ('Akdeniz', 'Panait Istrati', 13, 40),
+  ('Goriot Baba', 'Charles Dıckens ', 2, 50)
+  ;
+
+
+INSERT INTO CUSTOMERS (name, email) VALUES
+('Can Bonomo', 'can@gmail.com'),
+('Meg Myers', 'myers@gmail.com')
+;
+
+
+
+INSERT INTO ORDERS (status, total_amount) VALUES
+(1, 32.32),
+(1, 343.3)
+;
+
+
+
+INSERT INTO ORDER_ITEMS (order_id, book_id, book_count) VALUES
+(1l, 1l, 3),
+(1l, 2l, 4),
+(2l, 1l, 1)
+;
+
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,49 @@
+
+
+CREATE TABLE EMIN (
+                       id LONG AUTO_INCREMENT  PRIMARY KEY,
+                       title VARCHAR(250) NOT NULL,
+                       author_name VARCHAR(250) NOT NULL,
+                       count INTEGER DEFAULT NULL
+);
+
+
+DROP TABLE IF EXISTS BOOKS CASCADE;
+DROP TABLE IF EXISTS CUSTOMERS CASCADE;
+DROP TABLE IF EXISTS ORDERS CASCADE;
+DROP TABLE IF EXISTS ORDER_ITEMS CASCADE;
+
+
+CREATE TABLE BOOKS (
+  id LONG AUTO_INCREMENT  PRIMARY KEY,
+  title VARCHAR(250) NOT NULL,
+  author_name VARCHAR(250) NOT NULL,
+  price DECIMAL NOT NULL,
+  count INTEGER DEFAULT NULL
+);
+
+
+
+CREATE TABLE CUSTOMERS (
+                         id LONG AUTO_INCREMENT  PRIMARY KEY,
+                         name VARCHAR(250) NOT NULL,
+                         email VARCHAR(250) NOT NULL
+);
+
+
+CREATE TABLE ORDERS (
+                         id LONG AUTO_INCREMENT  PRIMARY KEY,
+                         status INTEGER NOT NULL,
+                         total_amount DOUBLE NOT NULL
+);
+
+
+
+
+CREATE TABLE ORDER_ITEMS (
+                        id LONG AUTO_INCREMENT  PRIMARY KEY,
+                        order_id LONG NOT NULL,
+                        book_id LONG NOT NULL,
+                        book_count DOUBLE NOT NULL
+);
+
